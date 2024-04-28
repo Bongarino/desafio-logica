d4c41c63936753fd086e02e5294881a16aa9caf3
Vitor Jacques <vitorjacques19@gmail.com>
2024-04-28 12:18:38 -0300

    Projeto Finalizado

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log("Ola mundo")
+// Definindo variáveis para o nome do herói e sua quantidade de experiência (XP)
+var nomeDoHeroi = "Felipão";
+var experienciaDoHeroi = 100001; // Exemplo de XP
+
+// Utilizando uma estrutura de decisão para determinar o nível do herói com base na quantidade de XP
+var nivelDoHeroi;
+
+if (experienciaDoHeroi < 1000) {
+    nivelDoHeroi = "Ferro";
+} else if (experienciaDoHeroi >= 1001 && experienciaDoHeroi <= 2000) {
+    nivelDoHeroi = "Bronze";
+} else if (experienciaDoHeroi >= 2001 && experienciaDoHeroi <= 5000) {
+    nivelDoHeroi = "Prata";
+} else if (experienciaDoHeroi >= 5001 && experienciaDoHeroi <= 7000) {
+    nivelDoHeroi = "Ouro";
+} else if (experienciaDoHeroi >= 7001 && experienciaDoHeroi <= 8000) {
+    nivelDoHeroi = "Platina";
+} else if (experienciaDoHeroi >= 8001 && experienciaDoHeroi <= 9000) {
+    nivelDoHeroi = "Ascendente";
+} else if (experienciaDoHeroi >= 9001 && experienciaDoHeroi <= 10000) {
+    nivelDoHeroi = "Imortal";
+} else {
+    nivelDoHeroi = "Radiante";
+}
+
+// Exibindo a mensagem com o nome e o nível do herói
+console.log("O Herói " + nomeDoHeroi + " está no nível " + nivelDoHeroi);
